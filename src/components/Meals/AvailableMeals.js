714f9c6fe48a184d771ae22a10c8096df9aaac89
parth-bhadra <63583662+parth-bhadra@users.noreply.github.com>
2022-05-18 18:28:36 +0530

    loading state and error state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,9 +6,16 @@ import 'dotenv/config';
 
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
+    const [isloading, setIsLoading] = useState(false);
+    const [httpError, setHttpError] = useState(null);
+
     useEffect(() => {
         const fetchMeals = async () => {
-            const response = await fetch(process.env.REACT_APP_FIREBASE);
+            setIsLoading(true);
+            const response = await fetch(process.env.REACT_APP_FIREBASE + 'bla');
+            if (!response.ok) {
+                throw new Error('something went wrong...');
+            }
             const responseData = await response.json();
             const loadedMeals = [];
 
@@ -22,9 +29,34 @@ const AvailableMeals = () => {
                 })
             }
             setMeals(loadedMeals);
-
+            setIsLoading(false);
+            setHttpError(null);
         }
-        fetchMeals();
+        // what happens here
+        // Uncaught (in promise) Error: something went wrong... at fetchMeals (AvailableMeals.js:17:1)
+        // why
+        // because fetch meals is an async function
+        // when error is thrown
+        // the async function rejects it 
+        // to capture the error, you need to await it
+        // but that is not possible because it would require us to alter the signature of our useEffect function
+        // which is not allowed
+        // useEffect fuctions cannot return a promise
+        // you can hover over and check it
+        // if you still want to go with try catch approach, you will have to wrap this in another function and call it
+
+        const fetchMealsAsyncWrapper = async () => {
+            try {
+                await fetchMeals();
+            } catch (error) {
+                console.log(error.message);
+                setIsLoading(false);
+                setHttpError(error.message);
+            }
+        }
+
+        fetchMealsAsyncWrapper();
+
 
     }, []);
     const mealsList = meals.map(meal => (
@@ -35,6 +67,23 @@ const AvailableMeals = () => {
             description={meal.description}
             price={meal.price}
         />))
+
+    if (isloading) {
+        return (
+            <section className={classes.MealsLoading}>
+                <p>loading...</p>
+            </section>
+        )
+    }
+
+    if (httpError) {
+        return (
+            <section className={classes.MealsError}>
+                <p>failed to load data...</p>
+            </section>
+        )
+    }
+
     return (
         <section className={classes.meals}>
             <Card>
